f2ee9ca6c3775a632cabcb69b9c6e207478819d2
Alexsandro Borges Correa <alexborgescorrrea@gmail.com>
2020-12-13 10:05:06 -0300

    Criado teste unitário do TaxaJurosController

---
+++ ApiTaxaJuros.Test/Controllers/TaxaJurosControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using ApiTaxaJuros.Aplicacao.TaxaJuros;
+using ApiTaxaJuros.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ApiTaxaJuros.Test.Controllers
+{
+    public class TaxaJurosControllerTest
+    {
+        [Fact]
+        public void TaxaPadrao_DeveRetornar00_1()
+        {
+            var taxaPadrao = 0.01m;
+            var mockTaxaJurosAplic = new Mock<ITaxaJurosAplic>();
+
+            mockTaxaJurosAplic.Setup(p => p.TaxaPadrao()).Returns(taxaPadrao);
+
+            var taxaJurosController = new TaxaJurosController(mockTaxaJurosAplic.Object);
+            var valorAtual = taxaJurosController.TaxaParao();
+
+            Assert.Equal(0.01m, valorAtual);
+        }
+    }
+}
